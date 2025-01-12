ed813bc7f4ed82cdfdc301c7f34e70aa2cf6ec23
Yurii <Jahnavart@gmail.com>
2025-01-12 13:47:05 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,82 +1,43 @@
-const sortByDescendingFriendCount = (users) => {
-  return users.toSorted((a, b) => b.friends.length - a.friends.length); 
-};
+class StringBuilder {
+    // Приватное свойство value
+  #value;
+    // Конструктор принимает начальное значение строки
+  constructor(initialValue) {
+    this.#value = initialValue; // Приватное свойство для хранения строки
+  }
 
+  // Метод для получения текущего значения
+  getValue() {
+    return this.#value; // Возвращаем текущее значение приватного свойства
+  }
 
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
-  ])
-);
-// 
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
+  // Метод для добавления строки в конец
+  padEnd(str) {
+    this.#value += str; // Добавляем строку в конец текущего значения через оператор +=
+    this.#value = this.#value + str; // Добавляем строку в конец текущего значения более гибче если потом надо будет добавлять еще что-то.
+  }
+
+  // Метод для добавления строки в начало
+  padStart(str) {
+    this.#value = str + this.#value; // Добавляем строку в начало текущего значения
+  }
+
+  // Метод для добавления строки с обеих сторон
+  padBoth(str) {
+    this.padStart(str); // Сначала добавляем строку в начало
+    this.padEnd(str);   // Затем добавляем строку в конец
+  }
+}
+
+// Код для проверки работы класса
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
